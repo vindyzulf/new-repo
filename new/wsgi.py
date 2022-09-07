0bf10b8a7f67fe0a94b94c5764893dd5a3d68d2c
vindyzulf <davina.fardya@ui.ac.id>
2022-09-07 19:42:56 +0700

    try

--- new/wsgi.py
+++ new/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for project0 project.
+WSGI config for new project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project0.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'new.settings')
 
 application = get_wsgi_application()
